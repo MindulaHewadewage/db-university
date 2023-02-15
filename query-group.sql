21767e0297d6b6591e20c1502bf5158520f426ae
MindulaHewadewage <mindufernando@gmail.com>
2023-02-15 14:54:21 +0100

    es 3 group

--- query-group.sql
+++ query-group.sql
@@ -3,5 +3,8 @@ SELECT COUNT(*) AS 'Num Studenti',YEAR(`enrolment_date`) AS 'anno iscrizione'
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(*) AS'Numero insegnanti' ,`office_address` AS'Indirizzo'
+FROM `teachers`
+GROUP BY `office_address`;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
