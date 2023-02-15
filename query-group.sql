a23a7898a92e6b0bcce99313e6961ad80749d481
MindulaHewadewage <mindufernando@gmail.com>
2023-02-15 14:59:49 +0100

    es 4 group

--- query-group.sql
+++ query-group.sql
@@ -7,4 +7,10 @@ SELECT COUNT(*) AS'Numero insegnanti' ,`office_address` AS'Indirizzo'
 FROM `teachers`
 GROUP BY `office_address`;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`,ROUND(AVG(`vote`)) AS 'Media Voti'
+FROM `exam_student`
+GROUP BY `exam_id`;
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT  COUNT(*) AS 'Num Corsi di Laurea',`department_id`
+FROM `degrees`
+GROUP BY `department_id`;
